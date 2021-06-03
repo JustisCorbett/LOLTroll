3200127b9f01347cc762552aae2c38bfd7acbe17
Justis Corbett <corbettjustis@gmail.com>
2021-06-03 16:52:19 -0600

    testing lol champion api

--- LOLTroll/buildgenerator/views.py
+++ LOLTroll/buildgenerator/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 # Create your views here.
 def index(request):
-    return HttpResponse("test")
+    r = requests.get('http://ddragon.leagueoflegends.com/cdn/11.11.1/data/en_US/champion.json')
+    data = r.json()
+    champions = data["data"]
+    print(champions["Akali"])
+    return HttpResponse(champions)
